3d40f05c0b30b1a212e87944ff161547f7cab226
xrdaukar <xrdaukar@gmail.com>
2024-06-07 11:24:06 -0700

    Disallow relative imports in LeMa (#65)
    -- Convert existing relative imports to absolute
    
    ---------
    
    Co-authored-by: xrdaukar <xrdaukar@google.com>

--- src/lema/builders/__init__.py
+++ src/lema/builders/__init__.py
@@ -1,6 +1,6 @@
-from .data import build_dataset
-from .models import build_model, build_peft_model, build_tokenizer
-from .training import build_trainer
+from lema.builders.data import build_dataset
+from lema.builders.models import build_model, build_peft_model, build_tokenizer
+from lema.builders.training import build_trainer
 
 __all__ = [
     "build_dataset",
