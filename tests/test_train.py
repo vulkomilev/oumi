81d0731dbb71f6e76e0a5fcefc3f8b72293326a8
xrdaukar <xrdaukar@gmail.com>
2024-06-12 10:48:00 -0700

    Switch to tempfile.TemporaryDirectory() in test_train.py (#75)
    Used as context manager, making sure temporary files are deleted after
    tests

--- tests/test_train.py
+++ tests/test_train.py
@@ -13,88 +13,85 @@ from lema.core.types import (
 
 
 def test_train_basic():
-    output_temp_dir = tempfile.mkdtemp()
+    with tempfile.TemporaryDirectory() as output_temp_dir:
+        config: TrainingConfig = TrainingConfig(
+            data=DataParams(
+                dataset_name="yahma/alpaca-cleaned",
+                preprocessing_function_name="alpaca",
+                text_col="prompt",
+            ),
+            model=ModelParams(
+                model_name="openai-community/gpt2",
+                model_max_length=1024,
+                trust_remote_code=True,
+            ),
+            training=TrainingParams(
+                trainer_type=TrainerType.TRL_SFT,
+                max_steps=3,
+                logging_steps=3,
+                enable_wandb=False,
+                enable_tensorboard=False,
+                output_dir=output_temp_dir,
+            ),
+        )
 
-    config: TrainingConfig = TrainingConfig(
-        data=DataParams(
-            dataset_name="yahma/alpaca-cleaned",
-            preprocessing_function_name="alpaca",
-            text_col="prompt",
-        ),
-        model=ModelParams(
-            model_name="openai-community/gpt2",
-            model_max_length=1024,
-            trust_remote_code=True,
-        ),
-        training=TrainingParams(
-            trainer_type=TrainerType.TRL_SFT,
-            max_steps=3,
-            logging_steps=3,
-            enable_wandb=False,
-            enable_tensorboard=False,
-            output_dir=output_temp_dir,
-        ),
-    )
-
-    train(config)
+        train(config)
 
 
 def test_train_custom():
-    output_temp_dir = tempfile.mkdtemp()
+    with tempfile.TemporaryDirectory() as output_temp_dir:
+        config: TrainingConfig = TrainingConfig(
+            data=DataParams(
+                dataset_name="yahma/alpaca-cleaned",
+                preprocessing_function_name="alpaca",
+                text_col="prompt",
+            ),
+            model=ModelParams(
+                model_name="learning-machines/sample",
+                tokenizer_name="gpt2",
+                model_max_length=1024,
+                trust_remote_code=False,
+            ),
+            training=TrainingParams(
+                trainer_type=TrainerType.TRL_SFT,
+                max_steps=3,
+                logging_steps=3,
+                enable_wandb=False,
+                enable_tensorboard=False,
+                output_dir=output_temp_dir,
+                include_performance_metrics=True,
+            ),
+        )
 
-    config: TrainingConfig = TrainingConfig(
-        data=DataParams(
-            dataset_name="yahma/alpaca-cleaned",
-            preprocessing_function_name="alpaca",
-            text_col="prompt",
-        ),
-        model=ModelParams(
-            model_name="learning-machines/sample",
-            tokenizer_name="gpt2",
-            model_max_length=1024,
-            trust_remote_code=False,
-        ),
-        training=TrainingParams(
-            trainer_type=TrainerType.TRL_SFT,
-            max_steps=3,
-            logging_steps=3,
-            enable_wandb=False,
-            enable_tensorboard=False,
-            output_dir=output_temp_dir,
-            include_performance_metrics=True,
-        ),
-    )
-
-    train(config)
+        train(config)
 
 
 # Currently takes a long time to run because packing is very slow.
 # TODO: Change `skip` to `e2e` after #62 is fixed.
 @pytest.mark.skip
 def test_train_pack():
-    output_temp_dir = tempfile.mkdtemp()
+    with tempfile.TemporaryDirectory() as output_temp_dir:
+        config: TrainingConfig = TrainingConfig(
+            data=DataParams(
+                dataset_name="Salesforce/wikitext",
+                dataset_config="wikitext-2-raw-v1",
+                stream=True,
+                pack=True,
+                text_col="text",
+            ),
+            model=ModelParams(
+                model_name="openai-community/gpt2",
+                model_max_length=1024,
+                trust_remote_code=True,
+            ),
+            training=TrainingParams(
+                trainer_type=TrainerType.TRL_SFT,
+                max_steps=1,
+                logging_steps=1,
+                enable_wandb=False,
+                enable_tensorboard=False,
+                output_dir=output_temp_dir,
+            ),
+        )
 
-    config: TrainingConfig = TrainingConfig(
-        data=DataParams(
-            dataset_name="Salesforce/wikitext",
-            dataset_config="wikitext-2-raw-v1",
-            stream=True,
-            pack=True,
-            text_col="text",
-        ),
-        model=ModelParams(
-            model_name="openai-community/gpt2",
-            model_max_length=1024,
-            trust_remote_code=True,
-        ),
-        training=TrainingParams(
-            trainer_type=TrainerType.TRL_SFT,
-            max_steps=1,
-            logging_steps=1,
-            enable_wandb=False,
-            enable_tensorboard=False,
-            output_dir=output_temp_dir,
-        ),
-    )
-
-    train(config)
+        train(config)
