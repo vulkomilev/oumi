4340fb667b1e8f821fb841dc7007fd8be14b6fbe
xrdaukar <xrdaukar@gmail.com>
2024-06-25 10:41:51 -0700

    Update sky_init.sh to print current dir (#120)

--- configs/skypilot/sky_init.sh
+++ configs/skypilot/sky_init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "SkyPilot node IPs: ${SKYPILOT_NODE_IPS}\n"
+echo "Current dir: $(pwd)"
+echo "SkyPilot node IPs: ${SKYPILOT_NODE_IPS}"
+echo ""
 echo "SkyPilot node rank: ${SKYPILOT_NODE_RANK}"
 export LEMA_NUM_NODES=`echo "$SKYPILOT_NODE_IPS" | wc -l`
 export LEMA_MASTER_ADDR=`echo "$SKYPILOT_NODE_IPS" | head -n1`
