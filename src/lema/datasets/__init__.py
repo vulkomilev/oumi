4b570db363c01904c963cf51bfbba8395a493989
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-07-10 08:27:47 -0700

    Add experimental notebook to run Nvidia's ChatRAG-Bench evaluation (#166)
    **Changes**
    - Add dataset loader class for `ChatRAG-Bench` subsets: `coqa`,
    `inscit`, `topiocqa`, `hybridial`, `doc2dial`, `quac`, `sqa`
    - Add notebook to run the evaluation
      - Lema's data loaders are used for pre-processing the dataset
      - vLLM is used for batched inference
      - Nvidia's script are used to compute the metrics
    - Move experimental folder outside of `src/lema`
    - Add an empty `SkyPilot` config to provision an node for manual
    debugging
    
    
    
    **Next Steps**
    Stopping this work stream for now, but for posterity if decide to resume
    this:
    - Add support for missing subsets: `qrecc`, `doqa_cooking`,
    `doqa_movies`, `doqa_travel`.
    - Add prompt used by Nvidia to train their models (should give more
    accurate metrics for their base model, but should not affect our
    results)
    
    
    Towards OPE-41

---
+++ src/lema/datasets/__init__.py
@@ -0,0 +1,5 @@
+from lema.datasets.alpaca import AlpacaDataset
+from lema.datasets.chatqa import ChatqaDataset
+from lema.datasets.chatrag_bench import ChatRAGBenchDataset
+
+__all__ = ["ChatRAGBenchDataset", "AlpacaDataset", "ChatqaDataset"]
