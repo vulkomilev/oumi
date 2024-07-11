9d5250a33a30a26e7c5cfb93bbb6b30572ce692f
xrdaukar <xrdaukar@gmail.com>
2024-07-11 14:37:34 -0700

    a

--- src/lema/train.py
+++ src/lema/train.py
@@ -106,6 +106,8 @@ def train(config: TrainingConfig, **kwargs) -> None:
     log_versioning_info()
     log_devices_info()
 
+    logger.info(f"Starting training: {config}\n")
+
     _ensure_training_output_dir_exists(config.training.output_dir)
 
     # Initialize model and tokenizer
