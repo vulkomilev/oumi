8fcd1e11bde1475814f528028b00f1fb4fb9a5f3
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-07-30 09:31:30 -0700

    Add initial docker image (#241)
    Adding a simple docker image with the lema dependencies.
    
    Towards OPE-52

---
+++ scripts/docker/build_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script to build the docker image and push it to the docker hub.
+
+set -xe
+
+DOCKER_HUB="TBD" # OPE-251
+VERSION=latest
+
+echo "Building docker image $DOCKER_HUB:$VERSION"
+docker build -t $DOCKER_HUB/lema:$VERSION .
+
+echo "Pushing docker image $DOCKER_HUB:$VERSION"
+docker push $DOCKER_HUB/lema:$VERSION
