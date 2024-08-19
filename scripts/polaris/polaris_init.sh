3119c4d35e83fe7c31b65168c3f986b9f075f125
xrdaukar <xrdaukar@gmail.com>
2024-08-19 15:57:58 -0700

    Update Polaris init script to print nodelist (#354)
    -- Also update `NCCL_DEBUG=` var in Launcher sample for consistency 
    
    Towards OPE-254

--- scripts/polaris/polaris_init.sh
+++ scripts/polaris/polaris_init.sh
@@ -11,6 +11,7 @@ export LEMA_NUM_NODES=`wc -l < $PBS_NODEFILE`
 export LEMA_MASTER_ADDR=`head -n1 $PBS_NODEFILE`
 echo "Master address: ${LEMA_MASTER_ADDR}"
 echo "Number of nodes: ${LEMA_NUM_NODES}"
+echo "All nodes: $(cat $PBS_NODEFILE)"
 
 if [[ -z "${LEMA_MASTER_ADDR}" ]]; then
     echo "Master address is empty!"
