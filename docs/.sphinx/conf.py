e57d82ffe9996c2925eaf172fbbaf189c8cb9825
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-08-26 09:28:28 -0700

    Enable markdown docs (#394)
    Enable importing markdown files when generating the documentation with
    sphinx. Markdown is enabled in addition to rest

--- docs/.sphinx/conf.py
+++ docs/.sphinx/conf.py
@@ -29,7 +29,13 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
+    "myst_parser",
 ]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
