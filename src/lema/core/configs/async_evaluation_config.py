9ef5c0888dd0413c34884f209bbf42cb29e48de8
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-08-27 16:41:41 -0700

    Update sphinx comments to docstrings (#411)
    `job_config.py` and `async_evaluation_config.py` already have comments,
    this PR reformats as docstrings.

--- src/lema/core/configs/async_evaluation_config.py
+++ src/lema/core/configs/async_evaluation_config.py
@@ -9,18 +9,27 @@ from lema.core.configs.evaluation_config import EvaluationConfig
 @dataclass
 class AsyncEvaluationConfig(BaseConfig):
     evaluation: EvaluationConfig = field(default_factory=EvaluationConfig)
+    """The evaluation configuration to use for each checkpoint.
 
-    #: The directory to poll for new checkpoints.
+    This field specifies the EvaluationConfig object that defines the parameters
+    for evaluating each checkpoint. It includes settings for the dataset,
+    model, generation, and evaluation framework to be used.
+    """
+
     checkpoints_dir: str = MISSING
+    """The directory to poll for new checkpoints."""
 
-    #: The time in seconds between the end of the previous evaluation and the start of
-    #: the next polling attempt. Cannot be negative.
     polling_interval: float = MISSING
+    """The time in seconds between the end of the previous evaluation and the start of
+    the next polling attempt. Cannot be negative.
+    """
 
-    #: The number of times to retry polling before exiting the current job.
-    #: A retry occurs when the job reads the target directory but cannot find a new
-    #: model checkpoint to evaluate. Defaults to 5. Cannot be negative.
     num_retries: int = 5
+    """The number of times to retry polling before exiting the current job.
+
+    A retry occurs when the job reads the target directory but cannot find a new
+    model checkpoint to evaluate. Defaults to 5. Cannot be negative.
+    """
 
     def __post_init__(self):
         """Verifies/populates params."""
--- src/lema/core/configs/job_config.py
+++ src/lema/core/configs/job_config.py
@@ -10,96 +10,127 @@ from lema.core.configs.base_config import BaseConfig
 class StorageMount:
     """A storage system mount to attach to a node."""
 
-    #: The remote path to mount the local path to (Required).
-    #: e.g. 'gs://bucket/path' for GCS, 's3://bucket/path' for S3, or 'r2://path' for
-    #: R2.
     source: str = MISSING
+    """The remote path to mount the local path to (Required).
 
-    #: The remote storage solution (Required). Must be one of 's3', 'gcs' or 'r2'.
+    e.g. 'gs://bucket/path' for GCS, 's3://bucket/path' for S3, or 'r2://path' for
+    R2.
+    """
+
     store: str = MISSING
+    """The remote storage solution (Required).
+
+    Must be one of 's3', 'gcs' or 'r2'.
+    """
 
 
 @dataclass
 class JobResources:
     """Resources required for a single node in a job."""
 
-    #: The cloud used to run the job (required).
     cloud: str = MISSING
+    """The cloud used to run the job (required)."""
 
-    #: The region to use (optional). Supported values vary by environment.
     region: Optional[str] = None
+    """The region to use (optional). Supported values vary by environment."""
 
-    #: The zone to use (optional). Supported values vary by environment.
     zone: Optional[str] = None
+    """The zone to use (optional). Supported values vary by environment."""
 
-    #: Accelerator type (optional). Supported values vary by environment.
-    #: For GCP you may specify the accelerator name and count, e.g. "V100:4".
     accelerators: Optional[str] = None
+    """Accelerator type (optional). Supported values vary by environment.
 
-    #: Number of vCPUs to use per node (optional). Sky-based clouds support strings with
-    #: modifiers, e.g. "2+" to indicate at least 2 vCPUs.
+    For GCP you may specify the accelerator name and count, e.g. "V100:4".
+    """
+
     cpus: Optional[str] = None
+    """Number of vCPUs to use per node (optional).
 
-    #: Memory to allocate per node in GiB (optional). Sky-based clouds support strings
-    #: with modifiers, e.g. "256+" to indicate at least 256 GB.
+    Sky-based clouds support strings with
+    modifiers, e.g. "2+" to indicate at least 2 vCPUs.
+    """
+
     memory: Optional[str] = None
+    """Memory to allocate per node in GiB (optional).
 
-    #: Instance type to use (optional). Supported values vary by environment.
-    #: The instance type is automatically inferred if `accelerators` is specified.
+    Sky-based clouds support strings
+    with modifiers, e.g. "256+" to indicate at least 256 GB.
+    """
+
     instance_type: Optional[str] = None
+    """Instance type to use (optional).
 
-    #: Whether the cluster should use spot instances (optional).
-    #: If unspecified, defaults to False (on-demand instances).
+    Supported values vary by environment.
+    The instance type is automatically inferred if `accelerators` is specified.
+    """
+
     use_spot: bool = False
+    """Whether the cluster should use spot instances (optional).
 
-    #: Disk size in GiB to allocate for OS (mounted at /). Ignored by Polaris. Optional.
+    If unspecified, defaults to False (on-demand instances).
+    """
+
     disk_size: Optional[int] = None
+    """Disk size in GiB to allocate for OS (mounted at /) (optional)
 
-    #: Disk tier to use for OS (optional).
-    #: For sky-based clouds this Could be one of 'low', 'medium', 'high' or 'best'
-    #: (default: 'medium').
+    Ignored by Polaris.
+    """
+
     disk_tier: Optional[str] = "medium"
+    """Disk tier to use for OS (optional).
+
+    For sky-based clouds this Could be one of 'low', 'medium', 'high' or 'best'
+    (default: 'medium').
+    """
 
 
 @dataclass
 class JobConfig(BaseConfig):
     """Configuration for launching jobs on a cluster."""
 
-    #: Job name (optional). Only used for display purposes.
     name: Optional[str] = None
+    """Job name (optional). Only used for display purposes."""
 
-    #: The user that the job will run as (optional). Required only for Polaris.
     user: Optional[str] = None
+    """The user that the job will run as (optional). Required only for Polaris."""
 
-    #: The local directory containing the scripts required to execute this job.
-    #: This directory will be copied to the remote node before the job is executed.
     working_dir: str = MISSING
+    """The local directory containing the scripts required to execute this job.
 
-    #: The number of nodes to use for the job. Defaults to 1.
+    This directory will be copied to the remote node before the job is executed.
+    """
+
     num_nodes: int = 1
+    """The number of nodes to use for the job. Defaults to 1."""
 
-    #: The resources required for each node in the job.
     resources: JobResources = field(default_factory=JobResources)
+    """The resources required for each node in the job."""
 
-    #: The environment variables to set on the node.
     envs: Dict[str, str] = field(default_factory=dict)
+    """The environment variables to set on the node."""
 
-    #: File mounts to attach to the node.
-    #: For mounting (copying) local directories, the key is the file path on the remote
-    #: and the value is the local path.
-    #: The keys of `file_mounts` cannot be shared with `storage_mounts`.
     file_mounts: Dict[str, str] = field(default_factory=dict)
+    """File mounts to attach to the node.
 
-    #: Storage system mounts to attach to the node.
-    #: For mounting remote storage solutions, the key is the file path on the remote
-    #: and the value is a StorageMount.
-    #: The keys of `storage_mounts` cannot be shared with `file_mounts`.
+    For mounting (copying) local directories, the key is the file path on the remote
+    and the value is the local path.
+    The keys of `file_mounts` cannot be shared with `storage_mounts`.
+    """
+
     storage_mounts: Dict[str, StorageMount] = field(default_factory=dict)
+    """Storage system mounts to attach to the node.
 
-    #: The setup script to run on every node. Optional.
-    #: `setup` will always be executed before `run`.
-    #: ex) pip install -r requirements.txt
+    For mounting remote storage solutions, the key is the file path on the remote
+    and the value is a StorageMount.
+    The keys of `storage_mounts` cannot be shared with `file_mounts`.
+    """
+
     setup: Optional[str] = None
+    """The setup script to run on every node. Optional.
 
-    #: The script to run on every node. Required. Runs after `setup`.
+    `setup` will always be executed before `run`.
+    ex) pip install -r requirements.txt
+    """
+
     run: str = MISSING
+    """The script to run on every node. Required. Runs after `setup`."""
