9c4dea7ccbfe134bc81d4061e5dfba3da2d96ee5
xrdaukar <xrdaukar@gmail.com>
2024-09-26 09:24:02 -0700

    Update experimental pretokenize_dataset tool (#554)
    -- Rename the tool to `process_dataset`, and add a new option to skip
    tokenization, in which case the tool downloads HF datasets, reshards it
    , it writes to GCS file system
    -- Add new sharding params
    -- Add new dataset params (`split`, `subset`, `trust_remote_code`)
    -- Add SkyPilot example how to download and save COCO dataset
    
    Towards OPE-102, OPE-353

--- src/experimental/pretokenize/process_dataset.py
+++ src/experimental/pretokenize/process_dataset.py
@@ -3,7 +3,18 @@ import functools
 import os
 import pathlib
 import time
-from typing import Any, Dict, Iterator, List, NamedTuple, Tuple, TypeVar, Union, cast
+from typing import (
+    Any,
+    Dict,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Tuple,
+    TypeVar,
+    Union,
+    cast,
+)
 
 import datasets
 from tqdm import tqdm
@@ -12,7 +23,7 @@ from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 from oumi.builders import (
     build_tokenizer,
 )
-from oumi.core.types import TrainingConfig
+from oumi.core.configs import TrainingConfig
 from oumi.utils.logging import logger
 
 _TOKEN_IDS_COLUMN_NAME = "input_ids"  # The common convention.
@@ -63,8 +74,8 @@ def _tokenize_dataset_impl(
     return dataset
 
 
-def _tokenize_file(
-    tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast],
+def _process_file(
+    tokenizer: Optional[Union[PreTrainedTokenizer, PreTrainedTokenizerFast]],
     target_col: str,
     input_file: pathlib.Path,
     input_format: str,
@@ -80,52 +91,116 @@ def _tokenize_file(
         assert input_format == "parquet"
         dataset = datasets.Dataset.from_parquet(str(input_file), keep_in_memory=True)
 
-    dataset = _tokenize_dataset_impl(
-        cast(datasets.Dataset, dataset),
-        tokenizer,
-        target_col,
-        num_proc=num_proc,
-        keep_in_memory=True,
-    )
+    if tokenizer is not None:
+        dataset = _tokenize_dataset_impl(
+            cast(datasets.Dataset, dataset),
+            tokenizer,
+            target_col,
+            num_proc=num_proc,
+            keep_in_memory=True,
+        )
 
-    logger.info(f"Writing the tokenized data to {output_parquet_file}.")
+    logger.info(f"Writing the processsed data to {output_parquet_file}.")
     dataset.to_parquet(output_parquet_file)
     logger.info(f"Finished writing to {output_parquet_file}.")
 
 
-def _tokenize_dataset(
-    tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast],
+def _process_dataset(
+    *,
+    tokenizer: Optional[Union[PreTrainedTokenizer, PreTrainedTokenizerFast]],
     target_col: str,
-    input_dataset_path: pathlib.Path,
+    input_dataset: str,
+    dataset_subset: Optional[str],
+    dataset_split: Optional[str],
+    trust_remote_code: bool,
     output_dataset_path: pathlib.Path,
+    num_shards: int,
+    max_shard_size: str,
     num_proc: int,
 ) -> None:
-    logger.info(f"Loading {input_dataset_path}.")
-    dataset = datasets.Dataset.load_from_disk(str(input_dataset_path))
+    if (
+        input_dataset.startswith("/")
+        or input_dataset.startswith(".")
+        or input_dataset.startswith("~")
+    ):
+        input_dataset_path = pathlib.Path(input_dataset)
+        logger.info(f"Loading {input_dataset_path} from disk...")
+        dataset = datasets.Dataset.load_from_disk(str(input_dataset_path))
+    else:
+        logger.info(f"Loading {input_dataset} from HuggingFace...")
+        splits_or_dataset = datasets.load_dataset(
+            path=input_dataset,
+            name=(dataset_subset or None),
+            split=(dataset_split or None),
+            trust_remote_code=trust_remote_code,
+        )
 
-    dataset = _tokenize_dataset_impl(
-        cast(datasets.Dataset, dataset),
-        tokenizer,
-        target_col,
-        num_proc=num_proc,
-        keep_in_memory=False,
+        if isinstance(
+            splits_or_dataset, (datasets.IterableDataset, datasets.IterableDatasetDict)
+        ):
+            raise ValueError("IterableDataset is not supported with this class.")
+
+        if isinstance(splits_or_dataset, datasets.Dataset):
+            dataset = splits_or_dataset
+        elif dataset_split:
+            dataset = splits_or_dataset[dataset_split]
+        elif len(splits_or_dataset) == 1:
+            dataset = splits_or_dataset.values().__iter__().__next__()
+        else:
+            raise ValueError(
+                "Multiple splits found in the dataset. Please specify a single split. "
+                f"Available splits: {list(splits_or_dataset.keys())}"
+            )
+
+    logger.info(
+        "\n".join(
+            [
+                "Dataset Loaded!",
+                f"Split: {dataset.split}",
+                f"Version: {dataset.version}",
+                f"Dataset size: {dataset.dataset_size}",
+                f"Download size: {dataset.download_size}",
+                f"Size: {dataset.size_in_bytes} bytes",
+                f"Column names: {dataset.column_names}",
+            ]
+        )
     )
 
-    logger.info(f"Writing the tokenized dataset to {output_dataset_path}.")
-    dataset.save_to_disk(str(output_dataset_path), max_shard_size="1GB")
-    logger.info(f"Finished writing to {output_dataset_path}.")
+    if tokenizer is not None:
+        dataset = _tokenize_dataset_impl(
+            cast(datasets.Dataset, dataset),
+            tokenizer,
+            target_col,
+            num_proc=num_proc,
+            keep_in_memory=False,
+        )
+
+    logger.info(f"Writing the output dataset to {output_dataset_path} ...")
+    dataset.save_to_disk(
+        str(output_dataset_path),
+        num_shards=(num_shards if not max_shard_size and num_shards > 0 else None),
+        max_shard_size=(max_shard_size or None),
+        num_proc=num_proc,
+    )
+    logger.info(f"Finished writing to {output_dataset_path} !")
 
 
 class ParsedArgs(NamedTuple):
     config_path: str
     verbose: bool
     input_dataset: str
+    dataset_subset: str
+    dataset_split: str
+    trust_remote_code: bool
     input_paths: List[str]
     input_format: str
     target_col: str
     output_dir: str
+    num_shards: int
+    max_shard_size: str
     overwrite: bool
     num_proc: int
+    skip_tokenize: bool
 
 
 def parse_cli() -> Tuple[ParsedArgs, List[str]]:
@@ -135,7 +210,6 @@ def parse_cli() -> Tuple[ParsedArgs, List[str]]:
         "-c",
         "--config",
         default=None,
-        required=True,
         help="Path to the configuration file",
     )
     parser.add_argument(
@@ -146,7 +220,6 @@ def parse_cli() -> Tuple[ParsedArgs, List[str]]:
     parser.add_argument(
         "--target_col",
         type=str,
-        required=True,
         default="",
         help="Target text column to tokenize.",
     )
@@ -164,6 +237,15 @@ def parse_cli() -> Tuple[ParsedArgs, List[str]]:
             "If -1, then use all available CPU cores."
         ),
     )
+    parser.add_argument(
+        "--skip_tokenize",
+        action="store_true",
+        help=(
+            "Whether to skip tokenization. "
+            "Can be useful if you just want to copy a dataset, "
+            "or convert it from one format to another."
+        ),
+    )
 
     parser.add_argument(
         "--output_dir",
@@ -171,12 +253,39 @@ def parse_cli() -> Tuple[ParsedArgs, List[str]]:
         required=True,
         help="Path to the output directory.",
     )
+    parser.add_argument(
+        "--max_shard_size",
+        type=str,
+        default="",
+        help="Max shard size e.g., 256MB",
+    )
+    parser.add_argument(
+        "--num_shards",
+        type=int,
+        default=512,
+        help="Number of shards.",
+    )
 
     # Parameters to work with HF datasets
     parser.add_argument(
         "--input_dataset",
         type=str,
         help="Directory path to the input Huggingface dataset, or a dataset name.",
+    )
+    parser.add_argument(
+        "--dataset_subset",
+        type=str,
+        help="Subset of an input dataset",
+    )
+    parser.add_argument(
+        "--dataset_split",
+        type=str,
+        help="Split of an input dataset e.g., 'train'",
+    )
+    parser.add_argument(
+        "--trust_remote_code",
+        action="store_true",
+        help="Whether to trust remote code.",
     )
 
     # Parameters to work with individual files
@@ -200,12 +309,18 @@ def parse_cli() -> Tuple[ParsedArgs, List[str]]:
             config_path=args.config,
             verbose=args.verbose,
             input_dataset=args.input_dataset,
+            dataset_subset=args.dataset_subset,
+            dataset_split=args.dataset_split,
+            trust_remote_code=args.trust_remote_code,
             input_paths=args.input_path,
             input_format=args.input_format,
             target_col=args.target_col,
             output_dir=args.output_dir,
+            num_shards=args.num_shards,
+            max_shard_size=args.max_shard_size,
             overwrite=args.overwrite,
             num_proc=args.num_proc,
+            skip_tokenize=args.skip_tokenize,
         ),
         unknown,
     )
@@ -218,44 +333,50 @@ def main() -> None:
     logger.info(f"Parsed arguments: {parsed_args}")
     logger.info(f"Unknown arguments: {arg_list}")
 
-    config: TrainingConfig = TrainingConfig.from_yaml_and_arg_list(
-        parsed_args.config_path, arg_list, logger=logger
-    )
+    config: TrainingConfig = None
+    target_col: str = ""
+    tokenizer: Optional[Union[PreTrainedTokenizer, PreTrainedTokenizerFast]] = None
+    if not parsed_args.skip_tokenize:
+        config = TrainingConfig.from_yaml_and_arg_list(
+            parsed_args.config_path, arg_list, logger=logger
+        )
 
-    # Find first non-empty value as target column name.
-    target_col = next(
-        s
-        for s in [
-            parsed_args.target_col,
-            config.data.train.target_col,
-            config.data.validation.target_col,
-            config.data.test.target_col,
-            "text",
-        ]
-        if s
-    )
+        # Find first non-empty value as target column name.
+        target_col = next(
+            s
+            for s in [
+                parsed_args.target_col,
+                config.data.train.target_col,
+                config.data.validation.target_col,
+                config.data.test.target_col,
+                "text",
+            ]
+            if s
+        )
+        logger.info("Initializing the tokenizer...")
+        tokenizer = build_tokenizer(config.model)
 
     output_dir: pathlib.Path = pathlib.Path(parsed_args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
 
     start_time = time.time()
 
-    logger.info("Initializing the tokenizer...")
-
-    tokenizer = build_tokenizer(config.model)
-
     num_proc = (
         (os.cpu_count() or 1) if parsed_args.num_proc == -1 else parsed_args.num_proc
     )
 
     if parsed_args.input_dataset:
-        input_dataset_path: pathlib.Path = pathlib.Path(parsed_args.input_dataset)
-        logger.info(f"Processing the dataset {input_dataset_path}...")
-        _tokenize_dataset(
-            tokenizer,
-            target_col,
-            input_dataset_path,
-            output_dir,
+        logger.info(f"Processing the dataset {parsed_args.input_dataset}...")
+        _process_dataset(
+            tokenizer=tokenizer,
+            target_col=target_col,
+            input_dataset=parsed_args.input_dataset,
+            dataset_subset=parsed_args.dataset_subset,
+            dataset_split=parsed_args.dataset_split,
+            trust_remote_code=parsed_args.trust_remote_code,
+            output_dataset_path=output_dir,
+            num_shards=parsed_args.num_shards,
+            max_shard_size=parsed_args.max_shard_size,
             num_proc=num_proc,
         )
     else:
@@ -274,7 +395,7 @@ def main() -> None:
             if output_file.exists() and not parsed_args.overwrite:
                 logger.error(f"{output_file} already exists. Specify --overwrite.")
                 continue
-            _tokenize_file(
+            _process_file(
                 tokenizer,
                 target_col,
                 input_file,
@@ -285,7 +406,7 @@ def main() -> None:
 
     end_time = time.time()
     logger.info(
-        f"Finished tokenizing the dataset. Elapsed time: {end_time - start_time} sec!"
+        f"Finished processing the dataset. Elapsed time: {end_time - start_time} sec!"
     )
 
 
