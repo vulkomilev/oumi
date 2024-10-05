9cbb3703c45757d0df6ba5b65f7d57bda7312822
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-10-04 17:20:17 -0700

    Add dataset class for dolly dataset (#586)
    - Add dataset class for dolly dataset
    
    Closes OPE-430

--- src/oumi/datasets/__init__.py
+++ src/oumi/datasets/__init__.py
@@ -25,6 +25,7 @@ from oumi.datasets.alpaca import AlpacaDataset
 from oumi.datasets.chatqa import ChatqaDataset
 from oumi.datasets.chatrag_bench import ChatRAGBenchDataset
 from oumi.datasets.debug import DebugClassificationDataset, DebugPretrainingDataset
+from oumi.datasets.dolly import ArgillaDollyDataset
 from oumi.datasets.vision_language.coco_captions import COCOCaptionsDataset
 from oumi.datasets.vision_language.flickr30k import Flickr30kDataset
 from oumi.datasets.vision_language.vision_jsonlines import JsonlinesDataset
@@ -38,4 +39,5 @@ __all__ = [
     "COCOCaptionsDataset",
     "Flickr30kDataset",
     "JsonlinesDataset",
+    "ArgillaDollyDataset",
 ]
