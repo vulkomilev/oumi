4200aa6a999d6fbe82a000d8a0aff689a460aebe
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-10-07 12:59:54 -0700

    [Tiny] update missing dataset import (#599)
    - SftJson dataset was not being imported in `oumi.datasets`, causing the
    dataset not to be registered at runtime

--- src/oumi/datasets/__init__.py
+++ src/oumi/datasets/__init__.py
@@ -27,6 +27,7 @@ from oumi.datasets.chatrag_bench import ChatRAGBenchDataset
 from oumi.datasets.debug import DebugClassificationDataset, DebugPretrainingDataset
 from oumi.datasets.dolly import ArgillaDollyDataset
 from oumi.datasets.magpie import ArgillaMagpieUltraDataset, MagpieProDataset
+from oumi.datasets.sft_jsonlines import TextSftJsonLinesDataset
 from oumi.datasets.vision_language.coco_captions import COCOCaptionsDataset
 from oumi.datasets.vision_language.flickr30k import Flickr30kDataset
 from oumi.datasets.vision_language.vision_jsonlines import JsonlinesDataset
@@ -43,4 +44,5 @@ __all__ = [
     "ArgillaDollyDataset",
     "ArgillaMagpieUltraDataset",
     "MagpieProDataset",
+    "TextSftJsonLinesDataset",
 ]
