ae24c789357fc8e07461457639cf8c2751c8a5c2
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-10-24 09:32:47 -0700

    [bug] enable logger propagation so that pytest can capture them (#769)
    - The `caplog` fixture is very useful to test logging, but was always
    empty because we don't propagate the oumi logger
    - This PR adds a fixture that enables logger propagation during testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,22 @@ from pathlib import Path
 
 import pytest
 
+from oumi.utils.logging import get_logger
+
 
 @pytest.fixture
 def root_testdata_dir():
     return Path(__file__).parent / "testdata"
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_logging():
+    """Fixture to set up logging for all tests.
+
+    We want to propagate to the root logger so that
+    pytest caplog can capture logs, and we can test
+    logging for the default oumi logger.
+    """
+    logger = get_logger("oumi")
+    logger.propagate = True
+    return logger
