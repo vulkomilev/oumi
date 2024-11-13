7424250f0414e61a8d4a7c19ad8d8f79607d5130
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-11-13 10:00:46 -0800

    Add support for remote vllm inference engine (#845)
    # Description:
    
    <!--
    Thank you for contributing to Oumi! Before sending your PR out for
    review, please take a quick read through this template.
    
    When your PR is merged, its title will appear in our release notes. Make
    sure your title gives a clear description of your change!
    
    After you've updated your title, please replace this section with a
    detailed description of your change. Include as much context as possible
    so your reviewers can easily understand *what* you're changing and
    *why*.
    The more information you provide, the faster we can review your change!
    -->
    <!--↓↓↓↓↓↓↓↓↓↓ Describe your change below ↓↓↓↓↓↓↓↓↓↓-->
    - This PR adds a new `RemoteVLLMInferenceEngine` class that enables
    running inference against remote VLLM servers.
    - Compared to `RemoteInferenceEngine`, it supports the additional
    parameters that can be used by `vllm`, e.g. structured decoding
    
    #### Usage
    ```python
    from oumi.inference import RemoteVLLMInferenceEngine
    from oumi.core.configs import ModelParams, GenerationParams
    
    engine = RemoteVLLMInferenceEngine(
        ModelParams(
            model_name="your-model",
            remote_params={
                "api_base": "http://your-vllm-server",
            }
        )
    )
    ```
    
    <!--↑↑↑↑↑↑↑↑↑↑ Describe your change above ↑↑↑↑↑↑↑↑↑↑-->
    
    ## Related issues
    
    <!--
    Make sure to list any relevant related issues to your change. More often
    than not this will be the single issue fixed by your PR.
    -->
    <!--↓↓↓↓↓↓↓↓↓↓ List your related issues below ↓↓↓↓↓↓↓↓↓↓-->
    
    Fixes # (issue)
    
    <!--↑↑↑↑↑↑↑↑↑↑ List your related issues above ↑↑↑↑↑↑↑↑↑↑-->
    
    ## Before submitting
    - [ ] This PR only changes documentation. (You can ignore the following
    checks in that case)
    - [x] Did you read the [contributor
    guideline](https://github.com/oumi-ai/oumi/blob/main/CONTRIBUTING.md)
    Pull Request guidelines?
    - [x] Did you link the issue(s) related to this PR in the section above?
    - [x] Did you add / update tests where needed?
    
    
    ## Reviewers
    
    At least one review from a member of `oumi-ai/oumi-staff` is required.
    
    <!-- Add `oumi-ai/oumi-staff` as a reviewer when your PR is ready for
    review.
    
    You are also welcome to add individual members of `oumi-ai/oumi-staff`
    as reviewers.
    
    If no one has reviewed your PR after several days, feel free to add a
    comment tagging specific reviewers.
    
     -->

--- src/oumi/inference/__init__.py
+++ src/oumi/inference/__init__.py
@@ -8,15 +8,17 @@ from oumi.inference.gcp_inference_engine import GoogleVertexInferenceEngine
 from oumi.inference.llama_cpp_inference_engine import LlamaCppInferenceEngine
 from oumi.inference.native_text_inference_engine import NativeTextInferenceEngine
 from oumi.inference.remote_inference_engine import RemoteInferenceEngine
+from oumi.inference.remote_vllm_inference_engine import RemoteVLLMInferenceEngine
 from oumi.inference.sglang_inference_engine import SGLangInferenceEngine
 from oumi.inference.vllm_inference_engine import VLLMInferenceEngine
 
 __all__ = [
     "AnthropicInferenceEngine",
+    "GoogleVertexInferenceEngine",
     "LlamaCppInferenceEngine",
     "NativeTextInferenceEngine",
     "RemoteInferenceEngine",
+    "RemoteVLLMInferenceEngine",
     "SGLangInferenceEngine",
     "VLLMInferenceEngine",
-    "GoogleVertexInferenceEngine",
 ]
---
+++ src/oumi/inference/remote_vllm_inference_engine.py
@@ -0,0 +1,78 @@
+from typing import Any
+
+from typing_extensions import override
+
+from oumi.core.configs import GenerationParams
+from oumi.core.types.conversation import Conversation
+from oumi.inference.remote_inference_engine import RemoteInferenceEngine
+
+_CONTENT_KEY: str = "content"
+_ROLE_KEY: str = "role"
+
+
+class RemoteVLLMInferenceEngine(RemoteInferenceEngine):
+    """Engine for running inference against Remote vLLM."""
+
+    @override
+    def get_supported_params(self) -> set[str]:
+        """Returns a set of supported generation parameters for this engine."""
+        return {
+            "frequency_penalty",
+            "logit_bias",
+            "presence_penalty",
+            "remote_params",
+            "seed",
+            "stop_strings",
+            "temperature",
+            "top_p",
+            "guided_decoding",
+        }
+
+    @override
+    def _convert_conversation_to_api_input(
+        self, conversation: Conversation, generation_params: GenerationParams
+    ) -> dict[str, Any]:
+        """Converts a conversation to an OpenAI input.
+
+        Documentation: https://platform.openai.com/docs/api-reference/chat/create
+
+        Args:
+            conversation: The conversation to convert.
+            generation_params: Parameters for generation during inference.
+
+        Returns:
+            Dict[str, Any]: A dictionary representing the OpenAI input.
+        """
+        api_input = {
+            "model": self._model,
+            "messages": [
+                {
+                    _CONTENT_KEY: [self._get_content_for_message(message)],
+                    _ROLE_KEY: message.role.value,
+                }
+                for message in conversation.messages
+            ],
+            # "max_completion_tokens": generation_params.max_new_tokens,
+            "temperature": generation_params.temperature,
+            "top_p": generation_params.top_p,
+            "frequency_penalty": generation_params.frequency_penalty,
+            "presence_penalty": generation_params.presence_penalty,
+            "n": 1,  # Number of completions to generate for each prompt.
+            "seed": generation_params.seed,
+            "logit_bias": generation_params.logit_bias,
+        }
+
+        if generation_params.guided_decoding:
+            if generation_params.guided_decoding.json:
+                api_input["guided_json"] = generation_params.guided_decoding.json
+
+            elif generation_params.guided_decoding.regex is not None:
+                api_input["guided_regex"] = generation_params.guided_decoding.regex
+
+            elif generation_params.guided_decoding.choice is not None:
+                api_input["guided_choice"] = generation_params.guided_decoding.choice
+
+        if generation_params.stop_strings:
+            api_input["stop"] = generation_params.stop_strings
+
+        return api_input
