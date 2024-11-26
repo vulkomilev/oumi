180bf42ee670a1e93f4813bb86e6f5aa75c3f117
xrdaukar <xrdaukar@gmail.com>
2024-11-26 11:19:42 -0800

    Add integration tests for visual SFT datasets (`VisionLanguageSftDataset`) (#863)
    # Description:
    
    <!--
    Thank you for contributing to Oumi! Before sending your PR out for
    review, please take a quick read through this template.
    
    When your PR is merged, its title will appear in our release notes. Make
    sure your title gives a clear description of your change!
    
    After you've updated your title, please replace this section with a
    detailed description of your change. Include as much context as possible
    so your reviewers can easily understand *what* you're changing and
    *why*.
    The more information you provide, the faster we can review your change!
    -->
    <!--↓↓↓↓↓↓↓↓↓↓ Describe your change below ↓↓↓↓↓↓↓↓↓↓-->
    
    -- Initial version, more to come.
    
    <!--↑↑↑↑↑↑↑↑↑↑ Describe your change above ↑↑↑↑↑↑↑↑↑↑-->
    
    ## Related issues
    
    <!--
    Make sure to list any relevant related issues to your change. More often
    than not this will be the single issue fixed by your PR.
    -->
    <!--↓↓↓↓↓↓↓↓↓↓ List your related issues below ↓↓↓↓↓↓↓↓↓↓-->
    
    Towards OPE-698
    
    <!--↑↑↑↑↑↑↑↑↑↑ List your related issues above ↑↑↑↑↑↑↑↑↑↑-->
    
    ## Before submitting
    - [ ] This PR only changes documentation. (You can ignore the following
    checks in that case)
    - [x] Did you read the [contributor
    guideline](https://github.com/oumi-ai/oumi/blob/main/CONTRIBUTING.md)
    Pull Request guidelines?
    - [x] Did you link the issue(s) related to this PR in the section above?
    - [x] Did you add / update tests where needed?
    
    
    ## Reviewers
    
    At least one review from a member of `oumi-ai/oumi-staff` is required.
    
    <!-- Add `oumi-ai/oumi-staff` as a reviewer when your PR is ready for
    review.
    
    You are also welcome to add individual members of `oumi-ai/oumi-staff`
    as reviewers.
    
    If no one has reviewed your PR after several days, feel free to add a
    comment tagging specific reviewers.
    
     -->

---
+++ tests/integration/datasets/test_sft_vision_datasets_load_datasets.py
@@ -0,0 +1,139 @@
+from typing import Final, NamedTuple, Optional
+
+import datasets
+import pytest
+
+from oumi.builders import (
+    build_dataset_mixture,
+    build_tokenizer,
+)
+from oumi.core.configs import (
+    DataParams,
+    DatasetParams,
+    DatasetSplit,
+    DatasetSplitParams,
+    ModelParams,
+    TrainingConfig,
+)
+from oumi.core.datasets import VisionLanguageSftDataset
+from oumi.core.registry import REGISTRY, RegistryType
+
+_DEFALT_DATASET_SPLIT: Final[str] = "test"
+_DEFAULT_MODEL_NAME: Final[str] = "meta-llama/Llama-3.2-11B-Vision-Instruct"
+_DEFAULT_CHAT_TEMPLATE: Final[str] = "llama3-instruct"
+
+
+def _get_all_sft_vision_dataset_names() -> list[str]:
+    """List all SFT datasets in the registry."""
+    datasets = []
+    for key, value in REGISTRY._registry.items():
+        if key.registry_type == RegistryType.DATASET and issubclass(
+            value, VisionLanguageSftDataset
+        ):
+            datasets.append(key.name)
+    return datasets
+
+
+class LoadDatasetInfo(NamedTuple):
+    dataset_name: str
+    model_name: str
+    max_rows: int = 32
+    extra_dataset_features: Optional[list[str]] = None
+    chat_template: str = _DEFAULT_CHAT_TEMPLATE
+    dataset_split: str = _DEFALT_DATASET_SPLIT
+    collator_name: str = "vision_language_with_padding"
+    trust_remote_code: bool = False
+
+
+def _get_all_sft_vision_dataset_infos() -> list[LoadDatasetInfo]:
+    # Special case datasets that should be excluded from default testing.
+    _EXCLUDED_DATASETS = set({"coco_captions", "vision_language_jsonl"})
+
+    all_dataset_names = set(_get_all_sft_vision_dataset_names())
+    result = [
+        LoadDatasetInfo(
+            dataset_name="merve/vqav2-small",
+            model_name=_DEFAULT_MODEL_NAME,
+            dataset_split="validation",
+            chat_template=_DEFAULT_CHAT_TEMPLATE,
+            trust_remote_code=True,
+            max_rows=32,
+        )
+    ]
+
+    manually_configured_dataset_names = set({info.dataset_name for info in result})
+    for dataset_name in all_dataset_names:
+        if (dataset_name in manually_configured_dataset_names) or (
+            dataset_name in _EXCLUDED_DATASETS
+        ):
+            continue
+        result.append(
+            LoadDatasetInfo(
+                dataset_name=dataset_name,
+                model_name=_DEFAULT_MODEL_NAME,
+                dataset_split=_DEFALT_DATASET_SPLIT,
+                chat_template=_DEFAULT_CHAT_TEMPLATE,
+                trust_remote_code=True,
+            )
+        )
+
+    assert len(result) > 1
+    for idx, info in enumerate(result):
+        assert info.dataset_name, f"Index: {idx}"
+        assert info.dataset_name in all_dataset_names, f"Index: {idx}"
+        assert info.model_name, f"Index: {idx}"
+        assert info.chat_template, f"Index: {idx}"
+        assert info.dataset_split, f"Index: {idx}"
+        assert info.collator_name, f"Index: {idx}"
+
+    return result
+
+
+@pytest.mark.parametrize("info", _get_all_sft_vision_dataset_infos())
+def test_build_dataset_mixture(info: LoadDatasetInfo):
+    debug_tag = f"Test: {info}"
+    model_params = ModelParams(
+        model_name=info.model_name,
+        trust_remote_code=info.trust_remote_code,
+        chat_template=info.chat_template,
+    )
+    tokenizer = build_tokenizer(model_params)
+    train_split = DatasetSplitParams(
+        collator_name=info.collator_name,
+        target_col="text",
+        datasets=[
+            DatasetParams(
+                dataset_name=info.dataset_name,
+                split=info.dataset_split,
+                shuffle=False,
+                seed=42,
+                trust_remote_code=info.trust_remote_code,
+                dataset_kwargs={
+                    "processor_name": info.model_name,
+                    "limit": info.max_rows,
+                    "return_tensors": True,
+                },
+            )
+        ],
+    )
+    train_config = TrainingConfig(
+        model=model_params, data=DataParams(train=train_split)
+    )
+    dataset = build_dataset_mixture(train_config, tokenizer, DatasetSplit.TRAIN)
+
+    assert isinstance(dataset, datasets.Dataset)
+
+    assert dataset.num_rows > 0, debug_tag
+    assert dataset.num_rows <= info.max_rows, debug_tag
+
+    assert "input_ids" in dataset.features, debug_tag
+    assert "attention_mask" in dataset.features, debug_tag
+    assert "pixel_values" in dataset.features, debug_tag
+    assert "labels" in dataset.features, debug_tag
+
+    if info.extra_dataset_features is not None and len(info.extra_dataset_features) > 0:
+        for extra_feature in info.extra_dataset_features:
+            assert extra_feature in dataset.features, debug_tag
+
+    assert dataset[0] is not None, debug_tag
+    assert dataset[dataset.num_rows - 1] is not None, debug_tag
