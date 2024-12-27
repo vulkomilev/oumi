d1e3f9a0cffb4cda9e214977681357d5e23debf5
Kostas <kaisopos@users.noreply.github.com>
2024-12-27 23:32:15 +0200

    Alpaca Eval: integration test (#945)
    # Description:
    
    <!--
    Thank you for contributing to Oumi! Before sending your PR out for
    review, please take a quick read through this template.
    
    When your PR is merged, its title will appear in our release notes. Make
    sure your title gives a clear description of your change!
    
    After you've updated your title, please replace this section with a
    detailed description of your change. Include as much context as possible
    so your reviewers can easily understand *what* you're changing and
    *why*.
    The more information you provide, the faster we can review your change!
    -->
    <!--↓↓↓↓↓↓↓↓↓↓ Describe your change below ↓↓↓↓↓↓↓↓↓↓-->
    Adding integration testing for Alpaca Eval. 
    
    <!--↑↑↑↑↑↑↑↑↑↑ Describe your change above ↑↑↑↑↑↑↑↑↑↑-->
    
    ## Related issues
    
    <!--
    Make sure to list any relevant related issues to your change. More often
    than not this will be the single issue fixed by your PR.
    -->
    <!--↓↓↓↓↓↓↓↓↓↓ List your related issues below ↓↓↓↓↓↓↓↓↓↓-->
    
    Fixes # N/A
    
    <!--↑↑↑↑↑↑↑↑↑↑ List your related issues above ↑↑↑↑↑↑↑↑↑↑-->
    
    ## Before submitting
    - [ ] This PR only changes documentation. (You can ignore the following
    checks in that case)
    - [ ] Did you read the [contributor
    guideline](https://github.com/oumi-ai/oumi/blob/main/CONTRIBUTING.md)
    Pull Request guidelines?
    - [ ] Did you link the issue(s) related to this PR in the section above?
    - [ ] Did you add / update tests where needed?
    
    
    ## Reviewers
    
    At least one review from a member of `oumi-ai/oumi-staff` is required.
    
    <!-- Add `oumi-ai/oumi-staff` as a reviewer when your PR is ready for
    review.
    
    You are also welcome to add individual members of `oumi-ai/oumi-staff`
    as reviewers.
    
    If no one has reviewed your PR after several days, feel free to add a
    comment tagging specific reviewers.
    
     -->

--- tests/integration/evaluate/test_evaluate.py
+++ tests/integration/evaluate/test_evaluate.py
@@ -1,7 +1,10 @@
 import json
 import os
 import tempfile
-import unittest
+from typing import Any
+from unittest.mock import patch
+
+import pandas as pd
 
 from oumi import evaluate
 from oumi.core.configs import (
@@ -12,104 +15,181 @@ from oumi.core.configs import (
 )
 from tests.markers import requires_gpus
 
-TASK = "mmlu_abstract_algebra"
-NUM_SAMPLES = 20
+# Currently supporting two platforms for evaluation: LM Harness and Alpaca Eval.
+LM_HARNESS = "lm_harness"
+ALPACA_EVAL = "alpaca_eval"
+
+TEST_TASK_PARAMS = {
+    LM_HARNESS: {
+        # EvaluationTaskParams params.
+        "task_name": "mmlu_abstract_algebra",
+        "num_samples": 20,
+        # ModelParams params.
+        "model_name": "openai-community/gpt2",
+        "model_max_length": 1,
+        # EvaluationConfig (remaining params needed).
+        "run_name": "test_lm_harness",
+    },
+    ALPACA_EVAL: {
+        # EvaluationTaskParams params.
+        "task_name": "",
+        "num_samples": 3,
+        # ModelParams params.
+        "model_name": "HuggingFaceTB/SmolLM2-135M-Instruct",
+        "model_max_length": 10,
+        # GenerationParams params (needed because Alpaca Eval runs Oumi inference).
+        "max_new_tokens": 10,
+        "batch_size": 1,
+        # EvaluationConfig (remaining params needed).
+        "run_name": "test_alpaca_eval",
+    },
+}
+
+EXPECTED_RESULTS = {
+    LM_HARNESS: {
+        ######### actual results returned by the `lm_eval.simple_evaluate` API ########
+        # {                                                                           #
+        #   "acc,none": 0.2,                                                          #
+        #   "acc_stderr,none": 0.09176629354822471,                                   #
+        #   "alias": "abstract_algebra"                                               #
+        # }                                                                           #
+        ###############################################################################
+        "acc,none": {"value": 0.2, "round_digits": 3},
+        "acc_stderr,none": {"value": 0.092, "round_digits": 3},
+    },
+    ALPACA_EVAL: {
+        ########## actual results returned by the `alpaca_eval.evaluate` API ##########
+        # {                                                                           #
+        #   "win_rate": 2.1546666665687532e-05,                                       #
+        #   "standard_error": 8.230602115518512e-06,                                  #
+        #   "n_wins": 0,                                                              #
+        #   "n_wins_base": 3,                                                         #
+        #   "n_draws": 0,                                                             #
+        #   "n_total": 3,                                                             #
+        #   "discrete_win_rate": 0.0,                                                 #
+        #   "mode": "community",                                                      #
+        #   "avg_length": 44,                                                         #
+        #   "length_controlled_winrate": 0.05370406224906349,                         #
+        #   "lc_standard_error": 0.014120007914531866,                                #
+        # }                                                                           #
+        ###############################################################################
+        "win_rate": {"value": 0.0000215, "round_digits": 7},
+        "standard_error": {"value": 0.0000082, "round_digits": 7},
+        "length_controlled_winrate": {"value": 0.054, "round_digits": 3},
+        "lc_standard_error": {"value": 0.014, "round_digits": 3},
+        "avg_length": {"value": 44, "round_digits": 0},
+    },
+    "alpaca_eval_model_outputs": [
+        # Expected responses from `SmolLM2-135M-Instruct` model (with max length = 10).
+        "Some of the most famous actors who started their careers",
+        "The process of getting a state's name is a",
+        "Absolutely, I'd be happy to help you",
+    ],
+}
+
+
+def _get_evaluation_config(platform: str, output_dir: str) -> EvaluationConfig:
+    evaluation_task_params = EvaluationTaskParams(
+        evaluation_platform=platform,
+        task_name=TEST_TASK_PARAMS[platform]["task_name"],
+        num_samples=TEST_TASK_PARAMS[platform]["num_samples"],
+    )
+    model_params = ModelParams(
+        model_name=TEST_TASK_PARAMS[platform]["model_name"],
+        model_max_length=TEST_TASK_PARAMS[platform]["model_max_length"],
+        trust_remote_code=True,
+    )
+    generation_params = (
+        GenerationParams(
+            max_new_tokens=TEST_TASK_PARAMS[platform]["max_new_tokens"],
+            batch_size=TEST_TASK_PARAMS[platform]["batch_size"],
+        )
+        if platform == ALPACA_EVAL
+        else GenerationParams()
+    )
+
+    return EvaluationConfig(
+        output_dir=output_dir,
+        tasks=[evaluation_task_params],
+        model=model_params,
+        generation=generation_params,
+        run_name=TEST_TASK_PARAMS[platform]["run_name"],
+    )
+
+
+def _validate_results(platform: str, output_dir: str) -> None:
+    # Identify the relevant output file: "<platform>_<timestamp>_results.json"
+    filenames = [f for f in os.listdir(output_dir) if f.endswith("_results.json")]
+    assert len(filenames) == 1
+    assert filenames[0].startswith(f"{platform}_")
+    output_path = os.path.join(output_dir, filenames[0])
+
+    # Read the results from the evaluation test's output file.
+    with open(output_path, encoding="utf-8") as file_ptr:
+        results_dict = json.load(file_ptr)["results"]
+        # Platforms with tasks (e.g. LM Harness) nest the results under the task names.
+        if "task_name" in TEST_TASK_PARAMS[platform]:
+            task_name = TEST_TASK_PARAMS[platform]["task_name"]
+            results_dict = results_dict[task_name] if task_name else results_dict
+
+    # Validate the results.
+    for expected_key in EXPECTED_RESULTS[platform]:
+        if expected_key not in results_dict:
+            raise ValueError(
+                f"Key `{expected_key}` was not found in the results: `{results_dict}`."
+            )
+        expected_value = EXPECTED_RESULTS[platform][expected_key]["value"]
+        round_digits = EXPECTED_RESULTS[platform][expected_key]["round_digits"]
+        actual_value = results_dict[expected_key]
+        if round(actual_value, round_digits) != expected_value:
+            raise ValueError(
+                f"Expected value for key `{expected_key}` should be `{expected_value}` "
+                f"(rounded to `{round_digits}` digits), but instead the actual value "
+                f"that was returned is `{actual_value}`."
+            )
+
+
+def _mock_alpaca_eval_evaluate(
+    model_outputs: dict[str, Any],
+    annotators_config: str,
+    fn_metric: str,
+    max_instances: int,
+    **kwargs,
+) -> tuple[pd.DataFrame, None]:
+    # Ensure the input arguments are the defaults (unless changed by this test).
+    assert annotators_config == "weighted_alpaca_eval_gpt4_turbo"
+    assert fn_metric == "get_length_controlled_winrate"
+    assert max_instances == TEST_TASK_PARAMS[ALPACA_EVAL]["num_samples"]
+
+    # Ensure the inference results (`model_outputs`) are the expected ones.
+    expected_model_outputs = EXPECTED_RESULTS["alpaca_eval_model_outputs"]
+    for i in range(TEST_TASK_PARAMS[ALPACA_EVAL]["num_samples"]):
+        if model_outputs["output"][i] != expected_model_outputs[i]:
+            raise ValueError(f"Unexpected model output: {model_outputs['output'][i]}")
+
+    # Mock the `alpaca_eval.evaluate` function (by returning the expected results).
+    expected_results_dict = EXPECTED_RESULTS[ALPACA_EVAL]
+    df_leaderboard = pd.DataFrame(
+        {key: expected_results_dict[key]["value"] for key in expected_results_dict},
+        index=[TEST_TASK_PARAMS[ALPACA_EVAL]["run_name"]],  # type: ignore
+    )
+    return df_leaderboard, None
 
 
 @requires_gpus()
 def test_evaluate_lm_harness():
     with tempfile.TemporaryDirectory() as output_temp_dir:
         nested_output_dir = os.path.join(output_temp_dir, "nested", "dir")
-
-        config: EvaluationConfig = EvaluationConfig(
-            output_dir=nested_output_dir,
-            tasks=[
-                EvaluationTaskParams(
-                    evaluation_platform="lm_harness",
-                    task_name=TASK,
-                    num_samples=NUM_SAMPLES,
-                )
-            ],
-            model=ModelParams(
-                model_name="openai-community/gpt2",
-                trust_remote_code=True,
-            ),
-        )
-
+        config = _get_evaluation_config(LM_HARNESS, nested_output_dir)
         evaluate(config)
-
-        # Identify the relevant output file: "lm_harness_<timestamp>_results.json"
-        files = [f for f in os.listdir(nested_output_dir) if f.endswith("results.json")]
-        assert len(files) == 1
-        output_file = os.path.join(nested_output_dir, files[0])
-
-        with open(output_file, encoding="utf-8") as f:
-            computed_metrics = json.load(f)
-            computed_metrics = computed_metrics["results"][TASK]
-            # expected metrics:
-            # {
-            #   "acc,none": 0.2,
-            #   "acc_stderr,none": 0.09176629354822471,
-            #   "alias": "abstract_algebra"
-            # }
-            assert round(computed_metrics["acc,none"], 3) == 0.2
-            assert round(computed_metrics["acc_stderr,none"], 3) == 0.092
+        _validate_results(platform=LM_HARNESS, output_dir=nested_output_dir)
 
 
 @requires_gpus()
-@unittest.skip("Skipping until we mock annotation (to avoid the cost of calling GPT4)")
 def test_evaluate_alpaca_eval():
     with tempfile.TemporaryDirectory() as output_temp_dir:
         nested_output_dir = os.path.join(output_temp_dir, "nested", "dir")
-
-        config: EvaluationConfig = EvaluationConfig(
-            output_dir=nested_output_dir,
-            tasks=[
-                EvaluationTaskParams(
-                    evaluation_platform="alpaca_eval",
-                    num_samples=3,
-                )
-            ],
-            model=ModelParams(
-                model_name="HuggingFaceTB/SmolLM2-135M-Instruct",
-                model_max_length=10,
-                trust_remote_code=True,
-            ),
-            generation=GenerationParams(
-                max_new_tokens=10,
-                batch_size=1,
-            ),
-        )
-
-        evaluate(config)
-
-        # Identify the relevant output file: "alpaca_eval_<timestamp>_results.json"
-        files = [f for f in os.listdir(nested_output_dir) if f.endswith("results.json")]
-        assert len(files) == 1
-        output_file = os.path.join(nested_output_dir, files[0])
-
-        with open(output_file, encoding="utf-8") as f:
-            computed_metrics = json.load(f)
-            computed_metrics = computed_metrics["results"]
-            # expected metrics:
-            # {
-            #   "win_rate": 2.1546666665687532e-05,
-            #   "standard_error": 8.230602115518512e-06,
-            #   "n_wins": 0,
-            #   "n_wins_base": 3,
-            #   "n_draws": 0,
-            #   "n_total": 3,
-            #   "discrete_win_rate": 0.0,
-            #   "mode": "community",
-            #   "avg_length": 44,
-            #   "length_controlled_winrate": 0.05370406224906349,
-            #   "lc_standard_error": 0.014120007914531866,
-            # }
-            assert round(computed_metrics["length_controlled_winrate"], 3) == 0.054
-            assert round(computed_metrics["lc_standard_error"], 3) == 0.014
-            assert round(computed_metrics["win_rate"], 7) == 0.0000215
-            assert round(computed_metrics["standard_error"], 7) == 0.0000082
-            assert computed_metrics["avg_length"] == 44
-            assert computed_metrics["n_wins"] == 0
-            assert computed_metrics["n_draws"] == 0
-            assert computed_metrics["n_total"] == 3
+        config = _get_evaluation_config(ALPACA_EVAL, nested_output_dir)
+        with patch("alpaca_eval.evaluate", _mock_alpaca_eval_evaluate):
+            evaluate(config)
+        _validate_results(platform=ALPACA_EVAL, output_dir=nested_output_dir)
