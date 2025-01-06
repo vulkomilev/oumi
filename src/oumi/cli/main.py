c7f979103fc0093ed86c4c3bed8a865a02da7dd6
Matthew Persons <matthew@oumi.ai>
2025-01-06 15:33:05 -0800

    Fix regression in CLI speed (#997)
    # Description
    
    <!--
    Thank you for contributing to Oumi! Before sending your PR out for
    review, please take a quick read through this template.
    
    When your PR is merged, its title will appear in our release notes. Make
    sure your title gives a clear description of your change!
    
    After you've updated your title, please replace this section with a
    detailed description of your change. Include as much context as possible
    so your reviewers can easily understand *what* you're changing and
    *why*.
    The more information you provide, the faster we can review your change!
    -->
    <!--↓↓↓↓↓↓↓↓↓↓ Describe your change below ↓↓↓↓↓↓↓↓↓↓-->
    
    Removed a dependency on `oumi.core` to speed up the CLI. This was
    introduced in #946
    Disabled our ASCII art for the distributed command.
    
    Before:
    ```
    time oumi       
    oumi  2.12s user 2.45s system 304% cpu 1.500 total
    ```
    
    After:
    ```
    time oumi
    oumi  0.09s user 0.03s system 97% cpu 0.129 total
    ```
    <!--↑↑↑↑↑↑↑↑↑↑ Describe your change above ↑↑↑↑↑↑↑↑↑↑-->
    
    ## Related issues
    
    <!--
    Make sure to list any relevant related issues to your change. More often
    than not this will be the single issue fixed by your PR.
    -->
    <!--↓↓↓↓↓↓↓↓↓↓ List your related issues below ↓↓↓↓↓↓↓↓↓↓-->
    
    Fixes # (issue)
    
    <!--↑↑↑↑↑↑↑↑↑↑ List your related issues above ↑↑↑↑↑↑↑↑↑↑-->
    
    ## Before submitting
    
    - [ ] This PR only changes documentation. (You can ignore the following
    checks in that case)
    - [x] Did you read the [contributor
    guideline](https://github.com/oumi-ai/oumi/blob/main/CONTRIBUTING.md)
    Pull Request guidelines?
    - [x] Did you link the issue(s) related to this PR in the section above?
    - [x] Did you add / update tests where needed?
    
    ## Reviewers
    
    At least one review from a member of `oumi-ai/oumi-staff` is required.
    
    <!-- Add `oumi-ai/oumi-staff` as a reviewer when your PR is ready for
    review.
    
    You are also welcome to add individual members of `oumi-ai/oumi-staff`
    as reviewers.
    
    If no one has reviewed your PR after several days, feel free to add a
    comment tagging specific reviewers.
    
     -->

--- src/oumi/cli/main.py
+++ src/oumi/cli/main.py
@@ -11,7 +11,6 @@ from oumi.cli.judge import conversations, dataset, model
 from oumi.cli.launch import cancel, down, status, stop, up, which
 from oumi.cli.launch import run as launcher_run
 from oumi.cli.train import train
-from oumi.core.distributed import is_world_process_zero
 
 _ASCII_LOGO = """
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -37,15 +36,15 @@ _ASCII_LOGO = """
 """
 
 
-def _oumi_welcome():
-    print(_ASCII_LOGO)
+def _oumi_welcome(ctx: typer.Context):
+    if ctx.invoked_subcommand != "distributed":
+        print(_ASCII_LOGO)
 
 
 def get_app() -> typer.Typer:
     """Create the Typer CLI app."""
     app = typer.Typer(pretty_exceptions_enable=False)
-    if is_world_process_zero():
-        app.callback()(_oumi_welcome)
+    app.callback()(_oumi_welcome)
     app.command(
         context_settings=CONTEXT_ALLOW_EXTRA_ARGS,
         help="Evaluate a model.",
