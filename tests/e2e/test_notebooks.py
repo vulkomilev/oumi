171b52990dd249a7fcd881abd070d7058a471575
wizeng23 <10782997+wizeng23@users.noreply.github.com>
2025-01-16 22:18:33 -0800

    Remove some notebook E2E tests (#1145)
    # Description
    
    This PR removes notebooks which are too expensive/slow to run for E2E
    tests (ex. they call GPT-4, they start a remote instance). In addition,
    it restores cell outputs for many of our notebooks.
    
    ## Related issues
    
    Towards OPE-713
    
    ## Before submitting
    
    - [ ] This PR only changes documentation. (You can ignore the following
    checks in that case)
    - [x] Did you read the [contributor
    guideline](https://github.com/oumi-ai/oumi/blob/main/CONTRIBUTING.md)
    Pull Request guidelines?
    - [x] Did you link the issue(s) related to this PR in the section above?
    - [x] Did you add / update tests where needed?

--- tests/e2e/test_notebooks.py
+++ tests/e2e/test_notebooks.py
@@ -8,11 +8,34 @@ from nbconvert.preprocessors import ExecutePreprocessor
 
 from oumi.utils.io_utils import get_oumi_root_directory
 
+# Notebooks whose tests we should skip, as they can't be run in an automated fashion.
+# Reasons include that they're too slow, they start remote jobs, etc.
+# An error will be thrown if a notebook in this set isn't found.
+_NOTEBOOKS_TO_SKIP = {
+    # Requires calling GPT-4 API
+    "Oumi - Evaluation with AlpacaEval 2.0.ipynb",
+    # Requires calling GPT-4 API
+    "Oumi - Evaluation with MT Bench.ipynb",
+    # Requires creating a remote instance
+    "Oumi - Running Jobs Remotely.ipynb",
+    # Requires creating a remote instance
+    "Oumi - Using vLLM Engine for Inference.ipynb",
+}
+
 
 def get_notebooks():
     """Get all notebooks in the notebooks directory."""
     notebooks_dir = get_oumi_root_directory().parent.parent / "notebooks"
-    return list(notebooks_dir.glob("*.ipynb"))
+    notebooks_to_skip = _NOTEBOOKS_TO_SKIP.copy()
+    notebooks_to_test = []
+    for notebook_path in notebooks_dir.glob("*.ipynb"):
+        if notebook_path.name in notebooks_to_skip:
+            notebooks_to_skip.remove(notebook_path.name)
+        else:
+            notebooks_to_test.append(notebook_path)
+    if notebooks_to_skip:
+        raise ValueError(f"Notebooks to skip not found: {notebooks_to_skip}")
+    return notebooks_to_test
 
 
 @pytest.mark.parametrize("notebook_path", get_notebooks(), ids=lambda x: x.name)
