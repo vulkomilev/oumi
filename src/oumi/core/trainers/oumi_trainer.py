6e2a3dacc590a3856faa4d66ebfade89f330a438
xrdaukar <xrdaukar@gmail.com>
2025-01-19 10:07:06 -0800

    OUMI Trainer: Support non-text models (#1175)

--- src/oumi/core/trainers/oumi_trainer.py
+++ src/oumi/core/trainers/oumi_trainer.py
@@ -62,7 +62,7 @@ class Trainer(BaseTrainer):
     def __init__(
         self,
         model: torch.nn.Module,
-        tokenizer: BaseTokenizer,
+        tokenizer: Optional[BaseTokenizer],
         args: TrainingParams,
         train_dataset: Dataset,
         processor: Optional[BaseProcessor] = None,
@@ -293,10 +293,14 @@ class Trainer(BaseTrainer):
 
                 # Count tokens on CPU.
                 with self._telemetry_block("computing tokens"):
-                    num_tokens = (
-                        batch["input_ids"].ne(self.tokenizer.pad_token_id).sum().item()
-                    )
-                    self.state.total_tokens_seen += num_tokens
+                    if self.tokenizer is not None and "input_ids" in batch:
+                        num_tokens = (
+                            batch["input_ids"]
+                            .ne(self.tokenizer.pad_token_id)
+                            .sum()
+                            .item()
+                        )
+                        self.state.total_tokens_seen += num_tokens
 
                 with self._telemetry_block("moving batch to device"):
                     if not self.is_using_fsdp and not self.is_using_ring_attention:
