349e47bd2034c50658ee61875c2de0f6d23aa857
Vishwanath Martur <64204611+vishwamartur@users.noreply.github.com>
2025-02-04 21:37:09 +0530

    Support HuggingFaceM4/Docmatix dataset (#1342)
    Co-authored-by: xrdaukar <xrdaukar@gmail.com>

---
+++ src/oumi/datasets/vision_language/docmatix.py
@@ -0,0 +1,70 @@
+# Copyright 2025 - Oumi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing_extensions import override
+
+from oumi.core.datasets import VisionLanguageSftDataset
+from oumi.core.registry import register_dataset
+from oumi.core.types.conversation import (
+    ContentItem,
+    Conversation,
+    Message,
+    Role,
+    Type,
+)
+
+
+@register_dataset("HuggingFaceM4/Docmatix")
+class DocmatixDataset(VisionLanguageSftDataset):
+    """Dataset class for the `HuggingFaceM4/Docmatix` dataset."""
+
+    default_dataset = "HuggingFaceM4/Docmatix"
+
+    @override
+    def transform_conversation(self, example: dict) -> Conversation:
+        """Transform a single conversation example into a Conversation object."""
+        input_text = example["question"]
+        output_text = example["answer"]
+
+        user_items: list[ContentItem] = []
+
+        if "image_bytes" in example:
+            user_items.append(
+                ContentItem(
+                    binary=example["image_bytes"],
+                    type=Type.IMAGE_BINARY,
+                )
+            )
+        elif "image_path" in example:
+            user_items.append(
+                ContentItem(
+                    content=example["image_path"],
+                    type=Type.IMAGE_PATH,
+                )
+            )
+        else:
+            raise ValueError(
+                "Training example contains none of required keys: "
+                "'image_bytes', 'image_path'. "
+                f"Available keys: {example.keys()}."
+            )
+
+        user_items.append(ContentItem(type=Type.TEXT, content=input_text))
+
+        return Conversation(
+            messages=[
+                Message(role=Role.USER, content=user_items),
+                Message(role=Role.ASSISTANT, content=output_text),
+            ]
+        )
